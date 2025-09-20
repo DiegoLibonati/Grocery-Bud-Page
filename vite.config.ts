8e5e2991835c20cbd2b3ab99390f7468c86674e3
Die <dlibonati@leafnoise.io>
2025-09-20 18:40:00 -0300

    feat: better code

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 // vite.config.ts
+import path from "path";
 import { defineConfig } from "vite";
-import path from "path"
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
-    },
-  },
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
       "@tests": path.resolve(__dirname, "./tests"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
 });
